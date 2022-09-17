ac18371e124173b54b76bcd4c5ae301ab3696c14
Anderson Marinho <marinho.1983@gmail.com>
2022-09-17 18:22:18 -0300

    teste rateio cromex1

--- rateioCromex.py
+++ rateioCromex.py
@@ -1,1 +1,19 @@
-print('Criando rateio de Cromex')
+# Layout Contabilização SAP lançamento de rateio
+
+import pandas as pd
+import numpy as np
+
+lista = ['31305035','60003', 'CR','Test Contabilização', '19089' ]
+
+campo1 = "E"
+campo2 = "0000"
+def campo3():
+    campo3 = "DB" or "CR"
+    if campo3 == lista[2]:
+        campo3 = "01"
+    else:
+        campo3 = "02"
+    return campo3
+campo3 = campo3()  
+
+print(campo1 + campo2 + campo3)
